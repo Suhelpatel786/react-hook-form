6817ed91c7d1dedf23413a847342444ec95d42c5
Suhel Patel <suhel.softcolon@gmail.com>
2024-01-31 22:01:46 +0530

    Basic Form setup with react-hook form use of register,control

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,30 +1,45 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
+let renderCount = 0;
 const YoutubeForm = () => {
+  const form = useForm();
+
+  const { register, control } = form;
+  renderCount++;
   return (
     <div>
+      {/* <h1>YouTube Form {renderCount / 2}</h1> */}
       <form>
         <label htmlFor="username">Username</label>
         <input
           placeholder="User name"
           type="text"
-          name="username"
           id="username"
+          {...register("username")}
         />
 
         <label htmlFor="email">Email</label>
-        <input placeholder="Email" type="email" name="email" id="email" />
+
+        <input
+          placeholder="Email"
+          {...register("email")}
+          type="email"
+          id="email"
+        />
 
         <label htmlFor="chanelname">Channel Name</label>
         <input
           placeholder="Chanel name"
           type="text"
-          name="chanelname"
+          {...register("chanelname")}
           id="chanelname"
         />
 
         <button type="submit">Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
